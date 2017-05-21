f78064a68285e8c6b409f5b911b2c0767064b6ba
Pier-Yves Lessard <py.lessard@gmail.com>
2017-05-20 23:51:56 -0400

    Added Apple Market spider

--- scrapyprj/database/forums/scripts/seed.sql
+++ scrapyprj/database/forums/scripts/seed.sql
@@ -3,7 +3,8 @@ insert ignore into `forum` (`name`, `spider`) values
 	('Dream Market Forum', 'dreammarket_forum'),
 	('Trade Route Forum', 'traderoute_forum'),
 	('RsClub Market Forum', 'rsclub_forum'),
-	('Darknet Heroes League', 'dhl_forum');
+	('Darknet Heroes League', 'dhl_forum'),
+	('Apple Market Forum', 'applemarket_forum');
 
 insert ignore into `user_propkey` (`name`, `prettyname`) values 
 	('joined_on', 'Joined on'),
@@ -17,7 +18,7 @@ insert ignore into `user_propkey` (`name`, `prettyname`) values
 	('location', 'Location'), 
 	('website', 'Website'), 
 	('signature', 'Signature'), 
-	('post_count', 'Page Count'), 
+	('post_count', 'Post Count'), 
 	('last_post', 'Last post on'),
 	('realname', 'Real name'),
 	('jabber', 'Jabber'),
