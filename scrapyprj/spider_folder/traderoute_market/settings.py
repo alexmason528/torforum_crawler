15f190835c54572e3a61e2a0c688f44966beb423
Pier-Yves Lessard <py.lessard@gmail.com>
2017-07-03 22:27:08 -0400

    Added traderoute market spider

---
+++ scrapyprj/spider_folder/traderoute_market/settings.py
@@ -0,0 +1,16 @@
+settings = {
+	'timezone' : 'UTC',							
+	'endpoint' : 'http://traderouteilbgzt.onion',
+	'prefix' : '',					
+	'resources' : {									
+		'index' : "/",
+		'loginpage' : '/login'
+		},
+	
+	'logins' : {				
+		'vivarossa1': {			
+			'username' : 'vivarossa1',
+			'password' : 'pass0000'
+		}
+	}
+}
