32897ba3515659c9f2c0ddc29bbb2ada6037bfb5
Pier-Yves Lessard <py.lessard@gmail.com>
2017-07-23 22:05:27 -0400

    Added support for wallstreet market and darknet heroes (with previous commit)

---
+++ scrapyprj/spider_folder/dhl_market/settings.py
@@ -0,0 +1,15 @@
+settings = {
+	'timezone' : 'UTC',							
+	'endpoint' : 'http://darkheroesq46awl.onion',
+	'prefix' : '',					
+	'resources' : {									
+		'index' : "/index",
+		},
+	
+	'logins' : {				
+		'vivarossa1': {			
+			'username' : 'malleybono',
+			'password' : 'pass0000'
+		}
+	}
+}
---
+++ scrapyprj/spider_folder/wallstreet_market/settings.py
@@ -0,0 +1,16 @@
+settings = {
+	'timezone' : 'UTC',							
+	'endpoint' : 'http://wallstyizjhkrvmj.onion',
+	'prefix' : '',					
+	'resources' : {									
+		'index' : "/",
+		'loginpage' : '/login'
+		},
+	
+	'logins' : {				
+		'vivarossa1': {			
+			'username' : 'vivarossa1',
+			'password' : 'pass0000'
+		}
+	}
+}
