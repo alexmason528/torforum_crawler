4ab574091fcd63a714c206633fb940c511765ce6
Steven Gilligan <steven@gilligan.io>
2017-12-09 14:20:44 -0500

    Aero forum crawler

---
+++ scrapyprj/spider_folder/aero_forum/settings.py
@@ -0,0 +1,16 @@
+settings = {
+	'timezone' : 'UTC',
+	'endpoint' : 'http://aerotalkgv5loftn.onion',
+	'prefix' : '',
+	'resources' : {
+		'loginpage' : 'login.php',
+		'index' : 'index.php'
+		},
+	
+	'logins' : {
+		'vivarossa1': {
+			'username' : 'vivarossa1',
+			'password' : 'pass0000'
+		}
+	}
+}
