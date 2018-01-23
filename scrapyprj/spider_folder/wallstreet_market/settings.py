6a3c7bf8eb48c3cc5edfb56fffc00a8c26f2ca25
Rasmus Munksgaard <rasmus.munksgaard.andersen@gmail.com>
2018-01-23 09:20:50 -0500

    Added 4 new user profiles.

--- scrapyprj/spider_folder/wallstreet_market/settings.py
+++ scrapyprj/spider_folder/wallstreet_market/settings.py
@@ -11,6 +11,22 @@ settings = {
 		'vivarossa1': {			
 			'username' : 'vivarossa1',
 			'password' : 'pass0000'
+		},
+		'atomscurl': {		
+			'username' : 'atomscurl',
+			'password' : 'pass0000'
+		},
+		'malleybono': {		
+			'username' : 'malleybono',
+			'password' : 'pass0000'
+		},
+		'poseidonsfeet': {		
+			'username' : 'poseidonsfeet',
+			'password' : 'pass0000'
+		},
+		'ocelotcylinders': {		
+			'username' : 'ocelotcylinders',
+			'password' : 'pass0000'
 		}
 	}
 }
