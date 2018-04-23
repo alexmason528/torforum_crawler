c67a1d90475c0080b38ac7274720cc2a78958ed6
rasmus_munksgaard <rasmus.munksgaard.andersen@gmail.com>
2018-04-23 08:58:17 -0400

    Spider with introductory debugging.

---
+++ scrapyprj/spider_folder/onionland_forum/settings.py
@@ -0,0 +1,24 @@
+settings = {
+	'timezone' : 'EST',
+	'endpoint' : 'http://onionlandbakyt3j.onion',
+	'prefix' : '',
+	'resources' : {
+		'loginpage' : 'index.php?action=login',
+		'index' : 'index.php'
+		},
+	
+	'logins' : {
+		'vivarossa1': {
+			'username' : 'vivarossa1',
+			'password' : 'pass0000'
+		},
+		'happyhippo': {
+			'username' : 'happyhippo',
+			'password' : 'pass0000'
+		},
+		'angrypenguin': {
+			'username' : 'angrypenguin',
+			'password' : 'pass0000'
+		}
+	}
+}
