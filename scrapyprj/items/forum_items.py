a1b6c6808d774e7b89c1990971d50224242e8bd6
rasmus_munksgaard <rasmus.munksgaard.andersen@gmail.com>
2018-04-27 10:27:07 -0400

    Added some comments on how to use items.

--- scrapyprj/items/forum_items.py
+++ scrapyprj/items/forum_items.py
@@ -43,12 +43,13 @@ class User(scrapy.Item):
 	website = scrapy.Field()
 
 	# Behavior.
-	post_count = scrapy.Field()
+	post_count = scrapy.Field() # This is the number of what we call threads.
 	last_post = scrapy.Field()
 	joined_on = scrapy.Field()
 	last_activity = scrapy.Field()
 	last_active = scrapy.Field() # Deprecated, do not use.
 	post_per_day = scrapy.Field()
+	message_count = scrapy.Field() # These are what we call messages. They are replies to threads.
 
 	# Communication options.
 	icq = scrapy.Field()
@@ -65,7 +66,6 @@ class User(scrapy.Item):
 	followers = scrapy.Field()
 	karma = scrapy.Field()
 	likes_received = scrapy.Field()
-	message_count = scrapy.Field()
 	rating_count = scrapy.Field()
 	reputation_power = scrapy.Field() # Use when it is calles reputation power.
 	rep_bars = scrapy.Field() # Use when it is called reputation bars or described as such.
