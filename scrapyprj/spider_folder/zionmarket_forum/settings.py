d3f6cc5bb6f9ac638104f60c4cbdb28b842fe2dc
rasmus_munksgaard <rasmus.munksgaard.andersen@gmail.com>
2018-04-28 16:26:59 -0400

    Added description of data and spider.

--- scrapyprj/spider_folder/zionmarket_forum/settings.py
+++ scrapyprj/spider_folder/zionmarket_forum/settings.py
@@ -1,3 +1,19 @@
+# Spider for Zion Market Forums. These are located on-market and do not foloow the standard board layout.
+# Because they are located on-market, vendor profiles can be connected to market data using the relativeurl. 
+# Since this data is already present on market, it is not necessary to yield these profiles and collect it.
+# Because the board follows a non-standard layout, we don't have typical user profiles. However, some user
+# data is available with each post and thread. Namely a link to the user, their member group and their "stars".
+# Rasmus is unsure whether stars actually indicate number of transactions, but this may be so. Many users have
+# 0 and it seems congruent with vendor sales.
+#
+# Two bugs are handled in-script. One is a race condition which occurs when Messages and Users are yielded in
+# rapid succession. Due to caching, one might reach the DB before the other which returns an error. To avoid 
+# this, we sleep for 0.5 seconds between each yield. If you encounter the error where an ID is read from the 
+# cache but not DB, try to increase this sleep time.
+# Another bug is yielding users without relative urls. Buyers do not have links, and can therefore not be
+# yielded with propvals (stars, member group). To do so anyways, we generate non-working URLs for fullurl 
+# and relativeurl which are endpoint+username and username.
+
 settings = {
 	'timezone' : 'UTC',
 	'endpoint' : 'http://zionshopusn6nopy.onion/',
