7c5c0aa2e78af28c18ad5f7e1d2a3fbcda1d2b8b
rasmus_munksgaard <rasmus.munksgaard.andersen@gmail.com>
2018-04-30 16:16:36 -0400

    Tidied up settings.

--- scrapyprj/spider_folder/cgmc_forum/settings.py
+++ scrapyprj/spider_folder/cgmc_forum/settings.py
@@ -13,16 +13,16 @@
 
 
 settings = {
-    'timezone': 'UTC',                             # Timezone used on the website.
-    'endpoint': 'http://forum.cgmcoopwhempo6a5.onion/',    # Endpoint of the site. Hostname only
+    'timezone': 'UTC',                             
+    'endpoint': 'http://forum.cgmcoopwhempo6a5.onion/',    
     'endpoint1': 'http://cgmcoopwhempo6a5.onion/',
-    'prefix': '',                  # http://myhost.onion/prefix1/prefixe2/profix3/page.php
-    'resources': {                                 # List of availables resources. Url can be created like   self.make_url('MyResource1')  = http://blahblahblah.onion/prefix1/prefix2/section1/page2.php
+    'prefix': '',                  
+    'resources': {                               
         'index': "/",
         'dologin': '/login.php'
         },
-    'logins': {          # Login information. They will be selected in a round robin fashioned when multiple spider is launched at once.
-        'malleybono': {  # The selected login information is avalaible through  self.login
+    'logins': {          
+        'malleybono': {  
             'username': 'malleybono',
             'password': 'pass0000'
         },
@@ -39,7 +39,6 @@ settings = {
             'password': 'pass0000'
         }
     },
-    # Exclude lists for URIs that we shouldn't visit
     'exclude': {
         'prefix': {  # Prefix exclude like '/account/' everything like '/account/logout' or '/account/settings' will be excluded
             # '/login',
