9416effc2a1ec0e15e45dfae7726140b4e24c949
rasmus_munksgaard <rasmus.munksgaard.andersen@gmail.com>
2018-05-01 00:44:31 -0400

    Revert "Updated version from Alex."
    This reverts commit 550e6d007c42e478e050d96b84765da936181bd1.

--- scrapyprj/spiders/ItalianDeepWebForumSpider.py
+++ scrapyprj/spiders/ItalianDeepWebForumSpider.py
@@ -35,11 +35,11 @@ class ItalianDeepWebForumSpider(ForumSpiderV3):
         self.set_max_concurrent_request(1)      # Scrapy config
         self.set_download_delay(10)             # Scrapy config
         self.set_max_queue_transfer_chunk(1)    # Custom Queue system
-        self.statsinterval = 60 				# Custom Queue system
-        self.logintrial = 0						# Max login attempts.
-        self.alt_hostnames = []					# Not in use.
-        self.report_status = True				# Report 200's.
-        self.loggedin = False					# Login flag. 
+        self.statsinterval = 60                 # Custom Queue system
+        self.logintrial = 0                     # Max login attempts.
+        self.alt_hostnames = []                 # Not in use.
+        self.report_status = True               # Report 200's.
+        self.loggedin = False                   # Login flag. 
 
     def start_requests(self):
         yield self.make_request(url = 'index', dont_filter=True)
@@ -64,8 +64,7 @@ class ItalianDeepWebForumSpider(ForumSpiderV3):
         if 'dont_filter' in kwargs:
             req.dont_filter = kwargs['dont_filter']
         if 'req_once_logged' in kwargs:
-            req.meta['req_once_logged'] = kwargs['req_once_logged']
-
+            req.meta['req_once_logged'] = kwargs['req_once_logged']  
         req.meta['proxy'] = self.proxy
         req.meta['slot'] = self.proxy
         req.meta['reqtype'] = reqtype   # We tell the type so that we can redo it if login is required
@@ -73,16 +72,14 @@ class ItalianDeepWebForumSpider(ForumSpiderV3):
 
     def parse_response(self, response):
         parser = None
-
         # Handle login status.
         if self.islogged(response) is False:
-            self.loggedin = False
+            self.loggedin = False              
+            req_once_logged = response.meta['req_once_logged'] if 'req_once_logged' in response.meta else response.request
             if self.is_login_page(response) is False:
                 # req_once_logged stores the request we will go to after logging in.
-                req_once_logged = response.request
                 yield self.make_request(reqtype='loginpage',response=response, req_once_logged=req_once_logged)
             else:
-                req_once_logged = response.meta['req_once_logged'] if 'req_once_logged'  in response.meta else response.request
                 # Try to yield informative error messages if we can't logon.
                 if self.is_login_page(response) is True and self.login_failed(response) is True:
                     self.logger.info('Failed last login as %s. Trying again' % self.login['username'])
@@ -103,154 +100,118 @@ class ItalianDeepWebForumSpider(ForumSpiderV3):
                     self.logger.warning("We are trying to yield a None. This should not happen.")
                 yield response.meta['req_once_logged']
                 self.loggedin = True
-
+            # else:
             if self.is_threadlisting(response) is True:
                 parser = self.parse_threadlisting
             elif self.is_message(response) is True:
-            	parser = self.parse_message
+                parser = self.parse_message
             elif self.is_user(response) is True:
-            	parser = self.parse_user
-
+                parser = self.parse_user
             #Yield the appropriate parsing function.
             if parser is not None:
                 for x in parser(response):
                     yield x
             else:
-                self.logger.warning("Unknown page type at %s" % response.url)
+                self.logger.info("Unknown page type at %s" % response.url)
 
     ########## PARSING FLAGS ##############
     def is_message(self, response):
-        return '/showthread.php' in response.url
+        if '/showthread.php?tid=' in response.url:
+            return True
 
     def is_user(self, response):
-        return '/member.php?action=profile&uid=' in response.url
+        if '/member.php?action=profile&uid=' in response.url:
+            return True
 
     def is_threadlisting(self, response):
-        return "/forumdisplay.php" in response.url
-
+        if "/forumdisplay.php?fid=" in response.url:
+            return True
     ########## PARSING FUNCTIONS ##########
     def parse_user(self, response):
-        # self.logger.info("Yielding profile from %s" % response.url)
+        self.logger.info("Yielding profile from %s" % response.url)
+        user = items.User()
+        user['relativeurl'] = urlparse(response.url).path
+        user['fullurl'] = response.url
+        user['username'] = self.get_text(response.css("fieldset table span.largetext em"))
+        
+        trs = response.css("table.tborder tbody tr")
 
-        try:
-            useritem                    = items.User()
+        for tr in trs:
+            if (len(tr.css('td')) == 2):
+                key = self.get_text(tr.css('td:first-child strong'))
+                content = self.get_text(tr.css('td:last-child'))
 
-            useritem['username']        = self.get_text(response.css("fieldset span.largetext span"))
-            useritem['relativeurl']     = urlparse(response.url).path
-            useritem['fullurl']         = response.url
-            useritem['username_id']     = self.get_url_param(response.url, 'uid')
+                if key == 'Iscritto dal:':
+                    user['joined_on'] = self.parse_timestr(content)
+                elif key == 'Ultima visita:':
+                    user['last_activity'] = self.parse_timestr(content)
+                elif key == 'Reputazione:':
+                    user['reputation'] = self.get_text(tr.css('td:last-child .reputation_neutral'))
+                elif key in ['avatar', 'pm']:
+                    pass
+                else:
+                    self.logger.warning('New information found on use profile page : "%s"' % key)
 
-            trs = response.css("#content div.wrapper table.tborder tr")
-
-            for tr in trs:
-                if (len(tr.css('td')) == 2):
-                    key = self.get_text(tr.css('td:first-child strong')).lower()
-                    content = self.get_text(tr.xpath('.//td[last()]/text()').extract_first())
-
-                    if key == 'joined:':
-                        useritem['joined_on'] = self.parse_timestr(content)
-                    elif key == 'last visit:':
-                        useritem['last_active'] = self.parse_timestr(content)
-                    elif key == 'total posts:':
-                        match = re.match('(\d+\.?\d*)', content)
-                        if match:
-                            useritem['message_count'] = match.group(1)
-                    elif key == 'total threads:':
-                        match = re.match('(\d+\.?\d*)', content)
-                        if match:
-                            useritem['post_count'] = match.group(1)
-                    elif key == 'reputation:':
-                        useritem['reputation'] = self.get_text(tr.css('.reputation_positive'))
-                    elif key == 'sex:':
-                        useritem['gender'] = content
-                    elif key in ['avatar', 'email:', 'private message:', 'bio:', 'time spent online:']:
-                        pass
-                    else:
-                        self.logger.warning('New information found on user profile page : "%s"' % key)
-
-            yield useritem
-
-        except Exception as e:
-            self.logger.warning("Cannot parse user item. %s" % e)
-            pass
-
+        yield user
 
     def parse_message(self, response):
-        # self.logger.info("Yielding messages from %s" % response.url)
-
-        threadid    = self.get_url_param(response.url, 'tid')
-        posts       = response.css("#posts .post")
-
+        self.logger.info("Yielding messages from %s" % response.url)
+        threadid =  self.get_url_param(response.url, 'tid')
+        posts = response.css("#post_container div.posts div.post")
         for post in posts:
-            if not 'deleted_post_hidden' in post.xpath('@class').extract_first():
-                try:
-                    post_date           = self.parse_timestr(self.get_text(post.css('span.post_date::text')))
-                    author_username     = self.get_text(post.css('.post_author .largetext a span'))
-                    contenttext         = post.css('.post_body')
-                    match               = re.match('post_(\d+)', post.xpath("@id").extract_first())
+            try:
+                messageitem = items.Message()
+                posttime = self.parse_timestr(self.get_text(post.css("span.post_date")))
 
-                    if match:
-                        post_id = match.group(1)
+                messageitem['author_username'] = self.get_text(post.css(".post_author .author_information span.largettext em").extract_first())
+                messageitem['postid'] = post.xpath("@id").extract_first()
+                messageitem['threadid'] = threadid
+                messageitem['posted_on'] = posttime
 
-                    messageitem                         = items.Message()
+                msg = post.css("div.post_body")
+                messageitem['contenttext'] = self.get_text(msg)
+                messageitem['contenthtml'] = self.get_text(msg.extract_first())
 
-                    messageitem['author_username']      = author_username
-                    messageitem['postid']               = post_id
-                    messageitem['threadid']             = threadid
-                    messageitem['posted_on']            = post_date
-                    messageitem['contenttext']          = self.get_text(contenttext)
-                    messageitem['contenthtml']          = contenttext.extract_first()
-
-                    yield messageitem
-
-                except Exception as e:
-                    self.logger.warning("Cannot parse message item. %s" % e)
-                    pass
+                yield messageitem
+            except Exception as e:
+                self.logger.warning("Invalid thread page. %s" % e)
 
     def parse_threadlisting(self, response):
-        # self.logger.info("Yielding threads from %s" % response.url)
-        
-        threads = response.css("#content tr.inline_row")
+        self.logger.info("Yielding threads from %s" % response.url)
+        for line in response.css("#content tbody tr.inline_row"):
+            threaditem = items.Thread()
+            threadlinkobj = line.css("td:nth-child(2) span.subject_new a") if len(line.css("td:nth-child(2) span.subject_new")) > 0 else line.css("td:nth-child(2) span.subject_old a")
+            last_post_time = self.parse_timestr(self.get_text(line.css("td:last-child span.lastpost")))
+            if threadlinkobj:
+                threadlinkhref = threadlinkobj.xpath("@href").extract_first() if threadlinkobj else None
+                threaditem['title'] = self.get_text(threadlinkobj)
+                threaditem['relativeurl'] = threadlinkhref
+                threaditem['fullurl']   = self.make_url(threadlinkhref)             
+                threaditem['threadid'] = self.get_url_param(threaditem['fullurl'], 'tid')
+                threaditem['author_username'] = self.get_text(line.css("td:nth-child(2) div.author a"))
+                threaditem['last_update'] = last_post_time
+                threaditem['replies']   = self.get_text(line.css("td:nth-child(4) a"))
+                threaditem['views']     = self.get_text(line.css("td:nth-child(5)"))
 
-        for thread in threads:
-            try:
-                threadlink          = thread.css("td:nth-child(3)").xpath(".//span[contains(@id, 'tid_')]/a")
-                threadurl           = threadlink.xpath('@href').extract_first()
-                lastpost_content    = self.get_text(thread.css("td:last-child span.lastpost"))
-                match               = re.search("(.+)Ultimo", lastpost_content)
+            self.logger.warning(threaditem)
 
-                if match:
-                    last_post_time  = self.parse_timestr(match.group(1))
-
-                threaditem                      = items.Thread()
-                threaditem['threadid'] 		    = self.get_url_param(threadurl, 'tid')
-                threaditem['title']			    = self.get_text(threadlink)
-                threaditem['relativeurl']       = threadurl
-                threaditem['fullurl']           = self.make_url(threadurl)
-                threaditem['author_username']   = self.get_text(thread.css("td:nth-child(3) div.author a"))
-                threaditem['last_update']       = last_post_time
-                threaditem['replies']           = self.get_text(thread.css("td:nth-child(4) a"))
-                threaditem['views']             = self.get_text(thread.css("td:nth-child(5)"))
-
-                yield threaditem
-
-            except Exception as e:
-                self.logger.error("Cannot parse thread item : %s" % e)
-                pass
+            yield threaditem
 
 
     ############ LOGIN HANDLING ################
     def login_failed(self, response):
-        return len(response.xpath('//div[@class="error"]')) > 0
+        if len(response.xpath('//div[@class="error"]')) > 0:
+            return True
 
     def islogged(self, response):
-        if 'Log Out' in response.body:
-            self.loggedin = True
+        if 'Log Out' in response.xpath('.//span[@class="welcome"]/a/text()').extract_first():
             return True
         return False
 
     def is_login_page(self, response):
-        return len(response.xpath('.//div[@class="wrapper"]//form[input[@value="do_login"]]')) > 0
+        if len(response.xpath('.//div[@class="wrapper"]//form[input[@value="do_login"]]')) > 0:
+            return True
+        return False
 
     def craft_login_request_from_form(self, response):
 
@@ -270,9 +231,6 @@ class ItalianDeepWebForumSpider(ForumSpiderV3):
     ########### MISCELLANEOUS ###################
     def parse_timestr(self, timestr):
         last_post_time = None
-        if timestr == '(Hidden)':
-            return last_post_time
-
         try:
             timestr = timestr.lower()
             timestr = timestr.replace('today', str(self.localnow().date()))
